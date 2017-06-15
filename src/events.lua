63fab052b15250a7ddcce402a746bd792c7c1972
Discookie <matekos17@fazekas.hu>
2017-06-15 09:45:59 +0200

    EventHandler init

--- src/events.lua
+++ src/events.lua
@@ -8,7 +8,43 @@ setmetatable(EventHandler, {
 
 function EventHandler.init()
     local self = setmetatable({}, EventHandler)
-    self.type = "Node"
-
+    self.type = "EventHandler"
+    self:reset()
     return self
+end
+
+function EventHandler:reset()
+    self._intervals
+end
+
+function EventHandler:add(event, callback)
+
+end
+
+function EventHandler:disable(id)
+
+end
+
+function EventHandler:enable(id)
+
+end
+
+function EventHandler:delete(id)
+    self:remove(id)
+end
+
+function EventHandler:remove(id)
+
+end
+
+function EventHandler:addInterval(seconds, callback)
+
+end
+
+function EventHandler:removeInterval(id)
+
+end
+
+function EventHandler:event(event)
+
 end
