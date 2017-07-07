9affd041cb368e4664f5f6d00589689df7ab9dd2
Discookie <matekos17@fazekas.hu>
2017-07-08 00:19:40 +0200

    Finish event frameworks

--- src/main.lua
+++ src/main.lua
@@ -11,7 +11,45 @@ require("node")
 
 -- Variable requires
 require("diff")
+require("track")
+require("traffic")
 -- End of variable requires
 
 require_d("test-data")
 
+function OnTrackCreated(track)
+    local ev = Event(Events.TRACK, track)
+    EventHandler.instance:throw(ev)
+end
+
+function OnFreqTrafficCreated(freqTraffic)
+    local ev = Event(Events.PRE_TRAFFIC, freqTraffic)
+    Eventhandler.instance:throw(ev)
+end
+
+function OnTrafficCreated(traffic)
+    local ev = Event(Events.TRAFFIC, traffic)
+    EventHandler.instance:throw(ev)
+end
+
+function OnRequestLoadObjects(track)
+    local ev = Event(Events.PRE_SKIN, track)
+    EventHandler.instance:throw(ev)
+end
+
+function OnSkinLoaded()
+    local ev = Event(Events.POST_SKIN)
+    EventHandler.instance:throw(ev)
+end
+
+function Update(dt, location, strafe, inputs, height)
+    local ev = Event(Events.FRAME, {dt = dt, location = location, strafe = strafe, inputs = inputs, height = height})
+    EventHandler.instance:throw(ev)
+end
+
+function OnRequestFinalScoring()
+    local ev = Event(Events.SCORE)
+    EventHandler.instance:throw(ev)
+end
+
+EventHandler.instance:throw(Event(Events.INIT))
