55fa8743cd29b64685923e7fa5bc5c023d437aa7
Discookie <matekos17@fazekas.hu>
2017-07-25 17:50:04 +0200

    Finish diff creation and create GameplaySettings

--- src/diff.lua
+++ src/diff.lua
@@ -21,6 +21,25 @@ Diff.JumpModes = {
     WAKE_BTN = "wake_or_button",
 }
 
+-- The groups do the same thing, just have different names for them
+Diff.GreyActions = {
+    CLOG      = "permanent"
+    STUCK     = "permanent"
+    PERMANENT = "permanent"
+
+    ERASE_ONE    = "erasesingle"
+    ERASE_SINGLE = "erasesingle"
+    ERASE_BLOCK  = "erasesingle"
+
+    ERASE_COLUMN = "erasecolumn"
+
+    ERASE_GRID = "eraseall"
+    ERASE_ALL  = "eraseall"
+
+    DEFAULT      = "default"
+    STAY_IN_GRID = "default"
+}
+
 Diff.defaults = {
     maxAccel  = 10,
     factAccel =  0.6,
@@ -93,6 +112,8 @@ Diff.defaults = {
     colorCount = 1,
     greyPercent = 0.3,
     randomGreys = false,
+    greyAction = Diff.GreyActions.DEFAULT,
+    greyRailBlocks = true,
 
     autoOverfillClear = false,
     clearOnlyOverfilledColumn = false,
@@ -103,6 +124,8 @@ Diff.defaults = {
     matchTime = 1.5,
 
     minBlockCount = 0,
+    minBestJumpTime = 2.5,
+
     greyType = 5,
     normalType = 6,
     invertType = 7,
@@ -113,18 +136,21 @@ Diff.defaults = {
     useCaterpillars = false,
 
     displayScoreboard = true,
-    showXInARowText = false,
+    showComboText = false,
 
     airStrafing = false,
     canCrash = false,
     canPush = false,
     canScoop = false,
     smoothStrafeToWake = false,
+    showboardPhysics = false,
 
     useJumpMarkers = false,
     minJumpTime = 2.5,
     jumpFixScaler = 1,
+    jumpHeightScaler = 1,
     minStrafeSpeed = -1,
+    maxStrafe =
     minAirTime = 0.9,
     minHeight = 3.5,
     autoStartJumps = true,
@@ -138,12 +164,25 @@ Diff.defaults = {
     launchMultiplier = 1,
     matchMultiplier = 1,
     stealthMultiplier = 1,
+    trickScoreWindow = true,
 
     startingScore = 0,
     ptsPerGray = 0,
     ptsPerColor = 0,
 
-    matchSizes = [],
+    trickPoints = {
+        400,
+        1000,
+        3000,
+        10000
+    },
+    trickDurations = {
+        1,
+        2,
+        3,
+        5
+    },
+    matchSizes = {},
     matchSize = 3,
 
     maxTricksPerJump = 99,
@@ -151,10 +190,15 @@ Diff.defaults = {
     finishMouseJumps = false,
     timeToSquat = 0,
 
+    autoCenter = true,
     gamepadRSSteering = false,
-    sideView = false,
 
-    towRopes = false
+    sideView = false,
+    compressTraffic = 0.65,
+    compressWater = 0.45,
+
+    numBinsForFreq = 256,
+    logSpacedFreqBins = false
 }
 
 function Diff.init(values)
@@ -192,8 +236,8 @@ end
 
 function Diff:loadValues(values)
     for (k, v) in pairs(values) do
-        if type(v) == "string" and (v == "" or v == "default") then
-            self[k] = Diff[k]
+        if type(v) == "nil" or (type(v) == "string" and (v == "" or v == "default")) then
+            self[k] = Diff.defaults[k]
         end
         self[k] = v
     end
@@ -212,4 +256,91 @@ function Diff:loadValues(values)
     self.maxAccel  = values.maxAccel  or (((values.maxAccelLeft  or self.maxAccel) + (values.maxAccelRight  or self.maxAccel)) / 2)
     self.factAccel = values.factAccel or (((values.factAccelLeft or self.maxAccel) + (values.factAccelRight or self.maxAccel)) / 2)
     self.minAccel  = values.minAccel  or (((values.minAccelLeft  or self.maxAccel) + (values.minAccelRight  or self.maxAccel)) / 2)
+end
+
+function Diff:doSettings()
+    GameplaySettings({
+            jumpheightscaler                    = self.jumpHeightScaler,
+            gravity                             = self.gravity,
+            allowmusicdroponfail                = self.musicCutOnFail,
+            preventautomaticoverfillclear       = self.autoOverfillClear,
+            usetraffic                          = self.useTraffic,
+            airstrafing                         = self.airStrafing,
+            usejumpmarkers                      = self.useJumpMarkers,
+            fullgrid_collectiongracetime        = self.fullGridGraceTime,
+            smoothstrafetowake                  = self.smoothStrafeToWake,
+            puzzleblockfallinterval             = self.blockFallTime,
+            blockflight_secondstopuzzle         = self.blockEnterTime,
+            calculate_antijumps_and_antitraffic = self.calcAntiTraffic,
+            automatic_traffic_collisions        = self.autoCollisions,
+            usepuzzlegrid                       = self.puzzleGrid,
+            puzzlerows                          = self.puzzleRows,
+            puzzlecols                          = self.puzzleCols,
+            cleargridonlanding                  = self.clearGridOnLanding,
+            forceclearsinglecolumns             = self.clearOnlyOverfilledColumn,
+            forcecollectiononoverfill           = self.autoOverfillClear,
+            show_x_in_a_row_text                = self.showComboText,
+            useBlockTrailToOptimalJumps         = self.useBlockTrailToOptimalJumps,
+            removeBlocksNearOptimalJumps        = self.removeBlocksNearOptimalJumps,
+            usePinatas                          = self.usePinatas,
+            useAirBlocks                        = self.useAirBlocks,
+            blocksInLanes                       = self.blocksInLanes,
+            useLivingScoreboardGhosts           = self.displayScoreboard,
+            cancrash                            = self.canCrash,
+            canpush                             = self.canPush,
+            canScoop                            = self.canScoop,
+            multilane                           = self.multiLane,
+            colorcount                          = self.colorCount,
+            blocktype_grey                      = self.greyType,
+            blocktype_highway                   = self.normalType,
+            blocktype_highwayinverted           = self.invertType,
+            rightsticksteering                  = self.gamepadRSSteering,
+            minvisibletrafficblocks             = self.minBlockCount,
+            greypercent                         = self.greyPercent,
+            greyrandomdistribution              = self.randomGreys,
+            usecaterpillars                     = self.useCaterpillars,
+            minimummultiplier                   = self.minMultiplier,
+            jumpautofixscaler                   = self.jumpFixScaler,
+            sideview                            = self.sideView,
+            minstrafespeedforwakejump           = self.minStrafeSpeed,
+            degreyatlandingzone                 = self.noGraysWhenLanding,
+            launchtrickmultiplier               = self.launchMultiplier,
+            maxnumtricksperjump                 = self.maxTricksPerJump,
+            autofinishgamepadjumps              = self.finishGamepadJumps,
+            autofinishmousejumps                = self.finishMouseJumps,
+            puzzlematchmultiplier               = self.matchMultiplier,
+            stealthscoremultiplier              = self.stealthMultiplier,
+            secondstoblockjumpsafterlanding     = self.timeToSquat,
+            easytraffic                         = self.easyTraffic,
+            autostartlaunchtricksforheldbuttons = self.autoStartJumps,
+            jumpmode                            = self.jumpMode,
+            minimumjump_airtime                 = self.minJumpTime,
+            minimumjump_height                  = self.minHeight,
+            startingscore                       = self.startingScore,
+            pointspergrey                       = self.ptsPerGrey,
+            pointspercolor                      = self.ptsPerColor,
+            matchcollectionseconds              = self.matchTime,
+            minmatchsizes                       = self.matchSize,
+            greyaction                          = self.greyAction,
+            usetrickscorewindow                 = self.trickScoreWindow,
+            trickdurations                      = self.trickDurations,
+            trickpoints                         = self.trickPoints,
+            playerminspeed                      = self.minSpeed,
+            playermaxspeed                      = self.maxSpeed,
+            minimumbestjumptime                 = self.minBestJumpTime,
+            uphilltiltscaler                    = self.uphillScale,
+            downhilltiltscaler                  = self.downhillScale,
+            uphilltiltsmoother                  = self.uphillSmoother,
+            downhilltiltsmoother                = self.downhillSmoother,
+            useadvancedsteepalgorithm           = self.advancedSteepAlgo,
+            alldownhill                         = self.downhillOnly,
+            railedblockscanbegrey               = self.greyRailBlocks,
+            trafficcompression                  = self.compressTraffic,
+            watercompression                    = self.compressWater,
+            autocenter                          = self.autoCenter,
+            -- maxstrafe                        = self.maxStrafe, -- is different when not set from mod
+            usesnowboardphysics                 = self.snowboardPhysics,
+            freqtrafficbins                     = self.numBinsForFreq,
+            freqtrafficbins_logspaced           = self.logSpacedFreqBins
+        })
 end
