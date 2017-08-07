aa6105734b2301eeecf56d154da2b3ea9d69768c
Discookie <matekos17@fazekas.hu>
2017-08-07 16:24:51 +0200

    Fix functions and commas in tables

--- src/utils/dump.lua
+++ src/utils/dump.lua
@@ -1,22 +1,35 @@
 function jsonify(object)
-    return _dump_recur(object, 0)
+    return _dump_recur(object)
 end
 
 function dump(object)
-    return _dump_recur(object, 0)
+    return _dump_recur(object)
 end
 
 function _dump_recur(obj, level)
-  local ret = "{"
-  if not level then level = 1 end
-  for k, v in pairs(obj) do
-    if type(v) == "table" then
-      ret = ret..tostring(k)..":".._dump_recur(v, level+1)..","
-    elseif type(v) == 'boolean' or type(v) == 'number' or type(v) == 'function' then
-      ret = ret..tostring(k)..": "..tostring(v)..","
-    else
-      ret = ret..tostring(k)..": \""..v.."\","
+    if not level then
+        level = 1
     end
-  end
-  return ret.."}"
+
+    local ret = "{"
+    local was = false
+
+    for k, v in pairs(obj) do
+        if was then
+            ret = ret .. ","
+        end
+
+        if type(v) == "table" then
+            ret = ret .. tostring(k) .. ":" .. _dump_recur(v, level+1)
+        elseif type(v) == 'boolean' or type(v) == 'number' then
+            ret = ret .. tostring(k) .. ": " .. tostring(v)
+        elseif type(v) == 'function' then
+            ret = ret .. tostring(k) .. ": \"" .. tostring(v) .. "\""
+        else
+            ret = ret .. tostring(k) .. ": \""..v.."\""
+        end
+
+        was = true
+    end
+    return ret.."}"
 end
