997f45c80e1ecb86cc6c6bc682143e19b49019de
Discookie <matekos17@fazekas.hu>
2017-08-14 17:51:04 +0200

    Add rounding utility

---
+++ src/utils/round.lua
@@ -0,0 +1,18 @@
+function math.round(number, precision)
+    if type(number) == "string" then
+        number = tonumber(number)
+    end
+    if type(number) ~= "number" then
+        return 0
+    end
+
+    if type(precision) == "string" then
+        precision = tonumber(precision)
+    end
+    if type(precision) ~= "number"
+        precision = 0
+    end
+
+    local tenk = math.pow(10, math.floor(precision))
+    return math.floor(number * tenk) / tenk
+end
