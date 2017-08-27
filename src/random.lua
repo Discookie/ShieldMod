461c2ae9544e4f74f15a1d0cce02a88a2d579b55
Discookie <matekos17@fazekas.hu>
2017-08-27 18:37:39 +0200

    Add random lib

---
+++ src/random.lua
@@ -0,0 +1,15 @@
+require("logger")
+require("track/track_export")
+
+function SetSeed(seed)
+    math.randomseed(seed)
+    Logger.Global:log("Setting random seed " .. seed)
+end
+
+local defaultSeed = 133742069
+SetSeed(defaultSeed)
+
+EventHandler.instance:on(Events.TRACK, function(ev)
+        SetSeed(Track.instance._time[Track.instance._size])
+    end
+)
