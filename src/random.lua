cafac3b267e1aa44a0e1bd7dccdad5d1e4470032
Discookie <matekos17@fazekas.hu>
2017-08-28 00:24:05 +0200

    Fix empty track bug

--- src/random.lua
+++ src/random.lua
@@ -2,14 +2,14 @@ require("logger")
 require("track/track_export")
 
 function SetSeed(seed)
+    Logger.Global:log("Setting random seed " .. tostring(seed))
     math.randomseed(seed)
-    Logger.Global:log("Setting random seed " .. seed)
 end
 
 local defaultSeed = 133742069
 SetSeed(defaultSeed)
 
-EventHandler.instance:on(Events.TRACK, function(ev)
+EventHandler.instance:on(Events.PRE_TRAFFIC, function(ev)
         SetSeed(Track.instance._time[Track.instance.size])
     end
 )
