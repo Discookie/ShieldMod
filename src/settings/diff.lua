ce4f1f4f7aabf0a0031075247e649e23689f5de1
Discookie <matekos17@fazekas.hu>
2017-09-11 23:51:46 +0200

    Add example diff settings

--- src/settings/diff.lua
+++ src/settings/diff.lua
@@ -1,5 +1,13 @@
 {
 useTraffic = false,
 puzzleGrid = false,
-jumpMode = Diff.JumpModes.NONE
+jumpMode = Diff.JumpModes.NONE,
+
+maxAccel  = 30,
+factAccel =  0.6,
+minAccel  =  0,
+
+maxDoubleSpan    = 3.0,
+minDoubleSpan    = 1.0,
+maxCrosshandSpan = 2.0
 }
