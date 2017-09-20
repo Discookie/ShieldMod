2cefd02b8d95839f2554f69ac0fad8829a44afa6
Discookie <matekos17@fazekas.hu>
2017-09-20 22:19:46 +0200

    BEGONE WHITE STUFF

--- src/camera.lua
+++ src/camera.lua
@@ -2,12 +2,13 @@ require("events")
 
 function SetCam()
     local logger = Logger("Camera")
+    HideBuiltinPlayerObjects()
     SetCamera({
 		pos = {0,2,-1.5},
 		rot = {-10,0,0},
 		railoffset = "detached"
 	})
-    logger:log("Camera override from mod")
+    logger:log("Camera and object override from mod")
 end
 
 EventHandler.instance:on(Events.POST_SKIN, SetCam)
