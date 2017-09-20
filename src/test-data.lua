69c0e6575eca814efb1446f64475e6a2d0383883
Discookie <matekos17@fazekas.hu>
2017-09-20 23:23:48 +0200

    Add mroe necessary definitions

--- src/test-data.lua
+++ src/test-data.lua
@@ -1,3 +1,7 @@
 GameplaySettings = GameplaySettings or function(asd) end
 
 GetMillisecondsSinceStartup = GetMillisecondsSinceStartup or function() end
+
+SetCamera = SetCamera or function(asd) end
+
+HideBuiltinPlayerObjects = HideBuiltinPlayerObjects or function() end
