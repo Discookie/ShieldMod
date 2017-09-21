6089e8eb943685918f46e63d09b2078e484d9b2f
Discookie <matekos17@fazekas.hu>
2017-09-21 19:50:41 +0200

    Your annual bugfixing batch

--- src/default-score.lua
+++ src/default-score.lua
@@ -2,7 +2,7 @@ require("logger")
 require("events")
 require("diff/diff_export")
 
-EventHandler.on(Events.SCORE, function(ev)
+EventHandler.instance:on(Events.SCORE, function(ev)
     if Diff.instance.scoreMode == "SHIELD" then
         AssignBuiltInAudioshieldScoring()
     end
