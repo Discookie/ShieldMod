109fc686ebf44ec38bd06cf409be29b86288a354
Discookie <matekos17@fazekas.hu>
2017-09-27 19:06:11 +0200

    Update spans

--- src/settings/diff.lua
+++ src/settings/diff.lua
@@ -7,7 +7,7 @@ maxAccel  = 30,
 factAccel =  0.6,
 minAccel  =  0,
 
-maxDoubleSpan    = 3.0,
-minDoubleSpan    = 1.0,
-maxCrosshandSpan = 2.0
+maxDoubleSpan    = 1.5,
+minDoubleSpan    = 0.5,
+maxCrosshandSpan = 1.0
 }
