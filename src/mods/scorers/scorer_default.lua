603274efbb980ad6a6748a4ac15b6b5fc10d05a4
Discookie <matekos17@fazekas.hu>
2017-09-28 20:38:36 +0200

    Add scorer logging

--- src/mods/scorers/scorer_default.lua
+++ src/mods/scorers/scorer_default.lua
@@ -1,9 +1,10 @@
-require("logger")
-require("events")
-require("diff/diff_export")
+require("../../logger")
+require("../../events")
+require("../../diff/diff_export")
 
 EventHandler.instance:on(Events.SCORE, function(ev)
     if Diff.instance.scoreMode == "SHIELD" then
+        Logger("Scorer_Default"):log("Set default Audioshield scoring")
         AssignBuiltInAudioshieldScoring()
     end
 end)
