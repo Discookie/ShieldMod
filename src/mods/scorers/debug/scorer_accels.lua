62c6f02bd854f8f029f8c0035d2c887d1da95e0e
Discookie <matekos17@fazekas.hu>
2017-10-15 19:20:13 +0200

    Transfer Accels over to using Diff to store settings

--- src/mods/scorers/debug/scorer_accels.lua
+++ src/mods/scorers/debug/scorer_accels.lua
@@ -32,22 +32,22 @@ sc_Accels_EventIDs = {}
 sc_Accels_Count = 0
 sc_Accels_Logger = Logger("AccelTracker")
 sc_Accels_TotalFailed = 0
-sc_Accels_Mode = "Avg"
-sc_Accels_Timer = 5
 
 function sc_Accels_RemoveNote(tid)
     local id = tid[1]
 	sc_Accels_Active[id] = nil
-	sc_Accels_RefreshScore()
+    if Diff.instance.debugAccelDisplay then
+	   sc_Accels_RefreshScore()
+    end
 	Intervals.instance:remove(sc_Accels_EventIDs[id])
 	sc_Accels_EventIDs[id] = nil
 end
 
 function sc_Accels_RefreshScore()
 	local final = 0
-	local isMax = sc_Accels_Mode == "Max"
-	local isAvg = sc_Accels_Mode == "Avg"
-	local isCurrent = sc_Accels_Mode == "Current"
+	local isMax = Diff.instance.debugAccelMode == "Max"
+	local isAvg = Diff.instance.debugAccelMode == "Avg"
+	local isCurrent = Diff.instance.debugAccelMode == "Current"
 	local fullCount = 0
 	local max = math.max
 	for k,v in pairs(sc_Accels_Active) do
@@ -72,12 +72,12 @@ function sc_Accels_RefreshScore()
 
     if final == 0 then
 	   SetScoreboardNote({
-			text = sc_Accels_Mode .. " Acceleration: " .. final,
+			text = Diff.instance.debugAccelMode .. " Acceleration: " .. final,
 			color = sc_Accels_Colors[-1]
 		})
     else
 	   SetScoreboardNote({
-			text = sc_Accels_Mode .. " Acceleration: " .. final,
+			text = Diff.instance.debugAccelMode .. " Acceleration: " .. final,
 			color = sc_Accels_Colors[math.floor(5*final/Diff.instance.maxAccel)]
 		})
     end
@@ -102,7 +102,7 @@ EventHandler.instance:on(Events.INIT, function(ev)
                         sc_Accels_Count = sc_Accels_Count + 1
                         sc_Accels_Active[sc_Accels_Count] = prevAccelLeft
                         sc_Accels_Overall[sc_Accels_Count] = prevAccelLeft
-                        sc_Accels_EventIDs[sc_Accels_Count] = Intervals.instance:addInterval(sc_Accels_Timer, true, sc_Accels_RemoveNote, {sc_Accels_Count})
+                        sc_Accels_EventIDs[sc_Accels_Count] = Intervals.instance:addInterval(Diff.instance.debugAccelTimer, true, sc_Accels_RemoveNote, {sc_Accels_Count})
 
                         if prevAccelLeft > Diff.instance.maxAccelLeft then
                             sc_Accels_TotalFailed = sc_Accels_TotalFailed + 1
@@ -125,7 +125,7 @@ EventHandler.instance:on(Events.INIT, function(ev)
                         sc_Accels_Count = sc_Accels_Count + 1
                         sc_Accels_Active[sc_Accels_Count] = prevAccelRight
                         sc_Accels_Overall[sc_Accels_Count] = prevAccelRight
-                        sc_Accels_EventIDs[sc_Accels_Count] = Intervals.instance:addInterval(sc_Accels_Timer, true, sc_Accels_RemoveNote, {sc_Accels_Count})
+                        sc_Accels_EventIDs[sc_Accels_Count] = Intervals.instance:addInterval(Diff.instance.debugAccelTimer, true, sc_Accels_RemoveNote, {sc_Accels_Count})
 
                         if prevAccelRight > Diff.instance.maxAccelRight then
                             sc_Accels_TotalFailed = sc_Accels_TotalFailed + 1
@@ -136,7 +136,9 @@ EventHandler.instance:on(Events.INIT, function(ev)
                     end
                 end
 
-                sc_Accels_RefreshScore()
+                if Diff.instance.debugAccelDisplay then
+                    sc_Accels_RefreshScore()
+                end
                 return false
             end
         )
