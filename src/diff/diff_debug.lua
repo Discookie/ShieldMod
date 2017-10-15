ceedbf191106ea9524d62eda2db5331ab26608a1
Discookie <matekos17@fazekas.hu>
2017-10-15 19:20:43 +0200

    time -> timer

--- src/diff/diff_debug.lua
+++ src/diff/diff_debug.lua
@@ -5,7 +5,7 @@ Diff.defaults_debug = {
     -- Used in debug scoring modules
     debugAccelDisplay = false,
     debugAccelMode = "Current",
-    debugAccelTime = 1,
+    debugAccelTimer = 1,
     debugDoubleSpanDisplay = false,
     debugDoubleSpanMode = "Current",
     debugDoubleSpanTimer = 1
