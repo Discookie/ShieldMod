b2f71ea99a53a5e4812ef9e422c4df4e80dd1c56
Discookie <matekos17@fazekas.hu>
2017-11-20 22:48:10 +0100

    Copy settings directory by default and add folder copy switch

--- make.bat
+++ make.bat
@@ -4,12 +4,14 @@ echo Setting up workspace...
 echo ================
 if exist "output" del /F /S /Q output >nul 2>nul
 mkdir output >nul 2>nul
-xcopy /S /Q htp output >nul 2>nul
-xcopy /S /Q tt output >nul 2>nul
-xcopy /S /Q src output\src >nul 2>nul
+xcopy /S /Q /I htp output >nul 2>nul
+xcopy /S /Q /I tt output >nul 2>nul
+xcopy /S /Q /I src output\src >nul 2>nul
+xcopy /S /Q /I settings output\settings >nul 2>nul
 copy LICENSE output\LICENSE >nul 2>nul
 copy README.md "output\README" >nul 2>nul
 copy "settings\display.js" "output\dynamic\diff.js" >nul 2>nul
+echo.
 echo ================
 echo Compiling...
 echo ================
